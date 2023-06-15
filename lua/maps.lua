1f220eb06e69d18b8e8565043a3ca542be163e0e
Guilherme Arantes <gui@aula.ac.uk>
2023-06-15 19:06:09 -0300

    feat: replace colon with <cmd> so hop works in visual mode

--- lua/maps.lua
+++ lua/maps.lua
@@ -19,46 +19,47 @@ set('v', 'c', '"_c')
 set('v', 'C', 'c')
 
 -- save and quit buffers
-set('n', '<leader>ww', ':w<Return>', { silent = true })
-set('n', '<leader>qq', ':w<Return>:bd<Return>', { silent = true })
-set('n', '<leader>q!', ':bd!<Return>', { silent = true })
+set('n', '<leader>ww', '<cmd>w<Return>', { silent = true })
+set('n', '<leader>qq', '<cmd>w<Return><cmd>bd<Return>', { silent = true })
+set('n', '<leader>q!', '<cmd>bd!<Return>', { silent = true })
 
 -- split and move windows
-set('n', '<leader>ws', ':split<Return>', { silent = true })
-set('n', '<leader>wv', ':vsplit<Return>', { silent = true })
+set('n', '<leader>ws', '<cmd>split<Return>', { silent = true })
+set('n', '<leader>wv', '<cmd>vsplit<Return>', { silent = true })
 set('n', '<leader>wh', '<C-w>h', { silent = true })
 set('n', '<leader>wj', '<C-w>j', { silent = true })
 set('n', '<leader>wk', '<C-w>k', { silent = true })
 set('n', '<leader>wl', '<C-w>l', { silent = true })
 set('n', '<leader>wo', '<C-w>o', { silent = true })
-set('n', '<leader>wc', ':close<Return>', { silent = true })
+set('n', '<leader>wc', '<cmd>close<Return>', { silent = true })
 
 -- oil
-set('n', '-', ':Oil %:h<Return>', { silent = true })
-set('n', '<leader>e', ':Oil .<Return>', { silent = true })
+set('n', '-', '<cmd>Oil %<cmd>h<Return>', { silent = true })
+set('n', '<leader>e', '<cmd>Oil .<Return>', { silent = true })
 
 -- git
-set('n', '<leader>c', ':G<Return><C-w>o', { silent = true })
-set('n', '<C-b>', ':Gitsigns prev_hunk<Return>', { silent = true })
-set('n', '<C-n>', ':Gitsigns next_hunk<Return>', { silent = true })
+set('n', '<leader>c', '<cmd>G<Return><C-w>o', { silent = true })
+set('n', '<C-b>', '<cmd>Gitsigns prev_hunk<Return>', { silent = true })
+set('n', '<C-n>', '<cmd>Gitsigns next_hunk<Return>', { silent = true })
 
 -- hop
-set('n', 's', ':HopWordMW<Return>', { silent = true })
+set('n', 's', '<cmd>HopWordMW<Return>', { silent = true })
+set('v', 's', '<cmd>HopWordMW<Return>', { silent = true })
 
 -- telescope
-set('n', '<leader>f', ':Telescope find_files<Return>', { silent = true })
-set('n', '<leader>g', ':Telescope live_grep<Return>', { silent = true })
-set('n', '<leader>b', ':Telescope buffers<Return>', { silent = true })
-set('n', '<leader>v', ':Telescope diagnostics<Return>', { silent = true })
-set('n', '<leader>s', ':Telescope resume<Return>', { silent = true })
+set('n', '<leader>f', '<cmd>Telescope find_files<Return>', { silent = true })
+set('n', '<leader>g', '<cmd>Telescope live_grep<Return>', { silent = true })
+set('n', '<leader>b', '<cmd>Telescope buffers<Return>', { silent = true })
+set('n', '<leader>v', '<cmd>Telescope diagnostics<Return>', { silent = true })
+set('n', '<leader>s', '<cmd>Telescope resume<Return>', { silent = true })
 
 -- lsp
-set('n', '<leader>d', ':lua vim.lsp.buf.definition()<Return>', { silent = true })
-set('n', '<leader>i', ':lua vim.lsp.buf.implementation()<Return>', { silent = true })
-set('n', '<leader>h', ':lua vim.lsp.buf.hover()<Return>', { silent = true })
-set('n', '<leader>a', ':lua vim.lsp.buf.code_action()<Return>', { silent = true })
-set('n', '<leader>r', ':lua vim.lsp.buf.references()<Return>', { silent = true })
-set('n', '<leader>t', ':lua vim.lsp.buf.rename()<Return>', { silent = true })
-set('n', '<leader>F', ':lua vim.lsp.buf.format()<Return>', { silent = true })
-set('n', '<leader>z', ':lua vim.diagnostic.goto_prev()<Return>', { silent = true })
-set('n', '<leader>x', ':lua vim.diagnostic.goto_next()<Return>', { silent = true })
+set('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<Return>', { silent = true })
+set('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<Return>', { silent = true })
+set('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<Return>', { silent = true })
+set('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<Return>', { silent = true })
+set('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<Return>', { silent = true })
+set('n', '<leader>t', '<cmd>lua vim.lsp.buf.rename()<Return>', { silent = true })
+set('n', '<leader>F', '<cmd>lua vim.lsp.buf.format()<Return>', { silent = true })
+set('n', '<leader>z', '<cmd>lua vim.diagnostic.goto_prev()<Return>', { silent = true })
+set('n', '<leader>x', '<cmd>lua vim.diagnostic.goto_next()<Return>', { silent = true })
