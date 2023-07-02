8d7a9af3cb018ec843b6b46f3084d0b4beaea174
Guilherme Arantes <arantes.g@outlook.com>
2023-07-02 07:02:11 -0300

    fix: which-key require before plugin load was causing issues in new nvim environments

--- lua/maps.lua
+++ lua/maps.lua
@@ -28,46 +28,4 @@ set('n', '-', '<cmd>lua require("oil").open()<cr>', {
 set('n', 's', '<cmd>HopChar1<cr>', { silent = true, desc = 'Jump to char' })
 set('v', 's', '<cmd>HopChar1<cr>', { silent = true, desc = 'Jump to char' })
 
-local wk = require('which-key')
-
-wk.register({
-	['<leader>'] = {
-		Q = { '<cmd>bd!<cr>', 'Quit buffer (discard unsaved changes)' },
-		q = { '<cmd>w<cr><cmd>bd<cr>', 'Save and quit buffer' },
-		e = { '<cmd>Oil .<cr>', 'Explore project directory' },
-		r = { '<cmd>Telescope oldfiles<cr>', 'Open recent buffer' },
-		t = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename symbol' },
-		y = { '<cmd>lua vim.lsp.buf.format()<cr>', 'Format document' },
-		u = { '<cmd>lua vim.lsp.buf.references()<cr>', 'List references' },
-		i = { '<cmd>term<cr>', 'Open new terminal buffer' },
-		-- o = {},
-		-- p = {},
-		a = { '<cmd>wa<cr>', 'Save all buffers' },
-		s = { '<cmd>w<cr>', 'Save buffer' },
-		d = { '<cmd>lua vim.lsp.buf.definition()<cr>', 'Jump to definition' },
-		f = { '<cmd>Telescope find_files<cr>', 'Search file in current directory' },
-		g = { '<cmd>Telescope live_grep<cr>', 'Search file content in current directory' },
-		h = { '<cmd>lua vim.lsp.buf.hover()<cr>', 'Show documentation' },
-		j = { '<cmd>Gitsigns next_hunk<cr>', 'Jump to next git hunk' },
-		k = { '<cmd>Gitsigns prev_hunk<cr>', 'Jump to previous git hunk' },
-		-- l = {},
-		z = { '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Show possible actions' },
-		x = { '<cmd>lua vim.diagnostic.goto_prev()<cr>', 'Jump to previous diagnostic' },
-		c = { '<cmd>lua vim.diagnostic.goto_next()<cr>', 'Jump to next diagnostic' },
-		v = { '<cmd>Telescope diagnostics<cr>', 'List diagnostics' },
-		b = { '<cmd>Telescope buffers<cr>', 'List buffers' },
-		n = { '<cmd>G<cr><C-w>o', 'Open git client' },
-		m = { '<cmd>Telescope resume<cr>', 'Back to Telescope panel' },
-		w = {
-			name = '+windows',
-			s = { '<cmd>split<cr>', 'Create new window below' },
-			v = { '<cmd>vsplit<cr>', 'Create new window to the right' },
-			h = { '<C-w>h', 'Navigate to window in the left' },
-			j = { '<C-w>j', 'Navigate to window below' },
-			k = { '<C-w>k', 'Navigate to window above' },
-			l = { '<C-w>l', 'Navigate to window in the right' },
-			o = { '<C-w>o', 'Close all other windows' },
-			c = { '<C-w>c', 'Close this window' },
-		},
-	},
-})
+-- after/plugin/which-key.rc.lua for maps that start with the leader key
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/guiaula/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/guiaula/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/guiaula/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/guiaula/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/guiaula/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/guilherme/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/guilherme/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/guilherme/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/guilherme/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/guilherme/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,107 +76,107 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["hop.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/hop.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/hop.nvim",
     url = "https://github.com/phaazon/hop.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-dap"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/nvim-dap",
     url = "https://github.com/mfussenegger/nvim-dap"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["oil.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/oil.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/oil.nvim",
     url = "https://github.com/stevearc/oil.nvim"
   },
   ["onedark.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/onedark.nvim",
     url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-sleuth"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/vim-sleuth",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/vim-sleuth",
     url = "https://github.com/tpope/vim-sleuth"
   },
   ["vs-tasks.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/vs-tasks.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/vs-tasks.nvim",
     url = "https://github.com/EthanJWright/vs-tasks.nvim"
   },
   ["which-key.nvim"] = {
     loaded = true,
-    path = "/Users/guiaula/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    path = "/Users/guilherme/.local/share/nvim/site/pack/packer/start/which-key.nvim",
     url = "https://github.com/folke/which-key.nvim"
   }
 }
